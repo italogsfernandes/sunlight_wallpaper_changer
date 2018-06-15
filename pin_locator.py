df6a5ddcc32fe3e820f8b951f57279bb1cfb5949
Ítalo Fernandes <italogsfernandes@gmail.com>
2018-06-15 08:50:33 -0300

    script organizado

--- pin_locator.py
+++ pin_locator.py
@@ -1,8 +1,45 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy_toolbox import read_image, save_image
+from scipy import misc # Opening and clossing
+#------------------------------------------------------------------------------
+## Opening and writing to image files (2.6.1.)
+# ------------------------------------------------------------------------------
+def save_image(input_image,name=None,extension='.png',folder=None):
+    """
+    Save a image in a determined format and folder
+    """    
+    complete_file_name = name + extension
+    if not folder is None:
+        complete_file_name = folder + complete_file_name   
+    misc.imsave(complete_file_name, input_image) # uses the Image module (PIL)
 
+def read_image(name,extension='.png',folder=None):
+    """
+    Reads a image from file.
+    Other options
+    -------------
+    Opening raw files
+    >>> face_from_raw = np.fromfile('face.raw', dtype=np.uint8)
+    >>> face_from_raw.shape = (768, 1024, 3)
+    Need to know the shape and dtype of the image (how to separate data bytes).
+    
+    For large data, use np.memmap for memory mapping:
+    >>> face_memmap = np.memmap('face.raw', dtype=np.uint8, shape=(768, 1024, 3))
+    (data are read from the file, and not loaded into memory)
+   
+    Working on a list of image files
+    >> from glob import glob
+    >> filelist = glob('random*.png')
+    """
+    complete_file_name = name + extension
+    if not folder is None:
+       complete_file_name = folder + complete_file_name   
+    
+    output_image = misc.imread(complete_file_name)
+    return output_image
+ 
+ 
 cities_data = [
         { 
           'name' : 'Uberlândia',
@@ -24,7 +61,7 @@ cities_data = [
 ## Abrindo as imagens
 world_image = read_image('/home/italo/Images/Wallpapers/world','.jpg')
 #world_image = read_image('brasil_test_image','.jpg')
-pin_image = read_image('/home/italo/Images/Wallpapers/pin_icon(15x20)','.jpg')
+pin_image = read_image('/home/italo/Images/Wallpapers/pin_locator_files/pin_icon(15x20)','.jpg')
 rgb2gray = lambda x: np.dot(x[...,:3], [0.299, 0.587, 0.114])
 pin_image_gray = rgb2gray(pin_image)
 
@@ -69,7 +106,7 @@ from datetime import datetime
 img = Image.open("/home/italo/Images/Wallpapers/world_pinned.jpg")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
-font = ImageFont.truetype("/home/italo/Images/Wallpapers/micross.ttf", 20)
+font = ImageFont.truetype("/home/italo/Images/Wallpapers/pin_locator_files/micross.ttf", 20)
 for city in cities_data:
     ## Determinando o retangulo onde estarão os marcadores
     x = city['location_pixels'][0]
